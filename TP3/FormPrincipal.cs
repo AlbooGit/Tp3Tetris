b20eb0e12a77d214920cb927a6d19a2b666d7242
Keatrix <samuel.cloutier21@gmail.com>
2016-12-02 12:24:16 -0500

    After Merge Patch 1

--- TP3/FormPrincipal.cs
+++ TP3/FormPrincipal.cs
@@ -4,177 +4,147 @@ using System.Windows.Forms;
 
 namespace TP3
 {
-  public partial class FormPrincipal : Form
-  {
-    public FormPrincipal()
+    public partial class FormPrincipal : Form
     {
-      GenererTableauEtat(nbLignes, nbColonnes);
-      FormConfiguration config = new FormConfiguration(this);
-      InitializeComponent();
-    }
-
-    int[] blocActifY = new int[4];
-    int[] blocActifX = new int[4];
-    int[] ligneCourante = new int[4];
-    int[] colonneCourante = new int[4];
-
-
-    // Représentation visuelles du jeu en mémoire.
-    PictureBox[,] toutesImagesVisuelles = null;
-    public int nbLignes = 20;
-    public int nbColonnes = 10;
-    int[,] tableauEtats = new int[20, 10];
-
-    /// <summary>
-    /// Gestionnaire de l'événement se produisant lors du premier affichage 
-    /// du formulaire principal.
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private void frmLoad(object sender, EventArgs e)
-    {
-      // Ne pas oublier de mettre en place les valeurs nécessaires à une partie.
-      ExecuterTestsUnitaires();
-      InitialiserSurfaceDeJeu(nbLignes, nbColonnes);
-    }
-
-    public void InitialiserSurfaceDeJeu(int nbLignes, int nbCols)
-    {
-      // Création d'une surface de jeu 10 colonnes x 20 lignes
-      toutesImagesVisuelles = new PictureBox[nbLignes, nbCols];
-      tableauJeu.Controls.Clear();
-      tableauJeu.ColumnCount = toutesImagesVisuelles.GetLength(1);
-      tableauJeu.RowCount = toutesImagesVisuelles.GetLength(0);
-      for (int i = 0; i < tableauJeu.RowCount; i++)
-      {
-        tableauJeu.RowStyles[i].Height = tableauJeu.Height / tableauJeu.RowCount;
-        for (int j = 0; j < tableauJeu.ColumnCount; j++)
+        public FormPrincipal()
         {
-          tableauJeu.ColumnStyles[j].Width = tableauJeu.Width / tableauJeu.ColumnCount;
-          // Création dynamique des PictureBox qui contiendront les pièces de jeu
-          PictureBox newPictureBox = new PictureBox();
-          newPictureBox.Width = tableauJeu.Width / tableauJeu.ColumnCount;
-          newPictureBox.Height = tableauJeu.Height / tableauJeu.RowCount;
-          newPictureBox.BackColor = Color.Black;
-          newPictureBox.Margin = new Padding(0, 0, 0, 0);
-          newPictureBox.BorderStyle = BorderStyle.FixedSingle;
-          newPictureBox.Dock = DockStyle.Fill;
-
-          // Assignation de la représentation visuelle.
-          toutesImagesVisuelles[i, j] = newPictureBox;
-          // Ajout dynamique du PictureBox créé dans la grille de mise en forme.
-          // A noter que l' "origine" du repère dans le tableau est en haut à gauche.
-          tableauJeu.Controls.Add(newPictureBox, j, i);
+            GenererTableauEtat(nbLignes, nbColonnes);
+            FormConfiguration config = new FormConfiguration(this);
+            InitializeComponent();
         }
-      }
-    }
+
+        int[] blocActifY = new int[4];
+        int[] blocActifX = new int[4];
+        int[] ligneCourante = new int[4];
+        int[] colonneCourante = new int[4];
 
 
-    enum TypeEtat
-    {
-      NONE,
-      GELE,
-      CARRE,
-      LIGNE,
-      T,
-      L,
-      J,
-      S,
-      Z
-    }
+        // Représentation visuelles du jeu en mémoire.
+        PictureBox[,] toutesImagesVisuelles = null;
+        public int nbLignes = 20;
+        public int nbColonnes = 10;
+        int[,] tableauEtats = new int[20, 10];
 
-    #region Code à développer
-    /// <summary>
-    /// Faites ici les appels requis pour vos tests unitaires.
-    /// </summary>
-    void ExecuterTestsUnitaires()
-    {
-      ExecuterTestABC();
-      // A compléter...
-    }
+        /// <summary>
+        /// Gestionnaire de l'événement se produisant lors du premier affichage 
+        /// du formulaire principal.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmLoad(object sender, EventArgs e)
+        {
+            // Ne pas oublier de mettre en place les valeurs nécessaires à une partie.
+            ExecuterTestsUnitaires();
+            InitialiserSurfaceDeJeu(nbLignes, nbColonnes);
+        }
 
-    // A renommer et commenter!
-    void ExecuterTestABC()
-    {
-      // Mise en place des données du test
+        public void InitialiserSurfaceDeJeu(int nbLignes, int nbCols)
+        {
+            // Création d'une surface de jeu 10 colonnes x 20 lignes
+            toutesImagesVisuelles = new PictureBox[nbLignes, nbCols];
+            tableauJeu.Controls.Clear();
+            tableauJeu.ColumnCount = toutesImagesVisuelles.GetLength(1);
+            tableauJeu.RowCount = toutesImagesVisuelles.GetLength(0);
+            for (int i = 0; i < tableauJeu.RowCount; i++)
+            {
+                tableauJeu.RowStyles[i].Height = tableauJeu.Height / tableauJeu.RowCount;
+                for (int j = 0; j < tableauJeu.ColumnCount; j++)
+                {
+                    tableauJeu.ColumnStyles[j].Width = tableauJeu.Width / tableauJeu.ColumnCount;
+                    // Création dynamique des PictureBox qui contiendront les pièces de jeu
+                    PictureBox newPictureBox = new PictureBox();
+                    newPictureBox.Width = tableauJeu.Width / tableauJeu.ColumnCount;
+                    newPictureBox.Height = tableauJeu.Height / tableauJeu.RowCount;
+                    newPictureBox.BackColor = Color.Black;
+                    newPictureBox.Margin = new Padding(0, 0, 0, 0);
+                    newPictureBox.BorderStyle = BorderStyle.FixedSingle;
+                    newPictureBox.Dock = DockStyle.Fill;
 
-      // Exécuter de la méthode à tester
+                    // Assignation de la représentation visuelle.
+                    toutesImagesVisuelles[i, j] = newPictureBox;
+                    // Ajout dynamique du PictureBox créé dans la grille de mise en forme.
+                    // A noter que l' "origine" du repère dans le tableau est en haut à gauche.
+                    tableauJeu.Controls.Add(newPictureBox, j, i);
+                }
+            }
+        }
 
-      // Validation des résultats
 
-      // Clean-up
-    }
+        enum TypeEtat
+        {
+            NONE,
+            GELE,
+            CARRE,
+            LIGNE,
+            T,
+            L,
+            J,
+            S,
+            Z
+        }
 
-    #endregion
+        #region Code à développer
+        /// <summary>
+        /// Faites ici les appels requis pour vos tests unitaires.
+        /// </summary>
+        void ExecuterTestsUnitaires()
+        {
+            ExecuterTestABC();
+            // A compléter...
+        }
 
-    private void configurationDuJeuToolStripMenuItem_Click(object sender, EventArgs e)
-    {
+        // A renommer et commenter!
+        void ExecuterTestABC()
+        {
+            // Mise en place des données du test
 
-    }
+            // Exécuter de la méthode à tester
 
-    public void configurationToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      FormConfiguration config = new FormConfiguration(this);
-      config.numericUpDown1.Value = nbLignes;
-      config.numericUpDown2.Value = nbColonnes;
-      config.ShowDialog();
-    }
+            // Validation des résultats
 
-    private void quittezToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      Application.Exit();
-    }
+            // Clean-up
+        }
 
-    private void réinitialiséPartieToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      InitialiserSurfaceDeJeu(nbLignes = 20, nbColonnes = 10);
-      GenererTableauEtat(nbLignes = 20, nbColonnes = 10);
-    }
+        #endregion
 
-    void InitialiserPieceDansTableau(int pieceRandom)
-    {
-      for (int i = 0; i < tableauEtats.GetLength(0); i++)
-      {
-        for (int j = 0; j < tableauEtats.GetLength(1); j++)
+        private void configurationDuJeuToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
-      }
+
+        public void configurationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormConfiguration config = new FormConfiguration(this);
+            config.numericUpDown1.Value = nbLignes;
+            config.numericUpDown2.Value = nbColonnes;
+            config.ShowDialog();
+        }
+
+        private void quittezToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void réinitialiséPartieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InitialiserSurfaceDeJeu(nbLignes = 20, nbColonnes = 10);
+            GenererTableauEtat(nbLignes = 20, nbColonnes = 10);
+        }
+
+        void InitialiserPieceDansTableau(int pieceRandom)
+        {
+            for (int i = 0; i < tableauEtats.GetLength(0); i++)
+            {
+                for (int j = 0; j < tableauEtats.GetLength(1); j++)
+                {
+
+                }
+            }
 
 
 
-    }
+        }
 
-    // <scloutier>
-    int GenererEtatPiece()
-    {
-      Random rnd = new Random();
-      int randomPiece = rnd.Next(2, 2);
-
-      if (randomPiece == 2) //Si bloc carré
-      {
-        blocActifY[0] = (int)TypeEtat.CARRE;
-        blocActifY[0] = (int)TypeEtat.CARRE;
-        blocActifY[1] = (int)TypeEtat.CARRE;
-        blocActifY[1] = (int)TypeEtat.CARRE;
-        blocActifX[0] = (int)TypeEtat.CARRE;
-        blocActifX[1] = (int)TypeEtat.CARRE;
-        blocActifX[0] = (int)TypeEtat.CARRE;
-        blocActifX[1] = (int)TypeEtat.CARRE;
-      }
-      else if (randomPiece == 3) //Si bloc ligne
-      {
-        blocActifY[0] = (int)TypeEtat.LIGNE;
-        blocActifY[0] = (int)TypeEtat.LIGNE;
-        blocActifY[0] = (int)TypeEtat.LIGNE;
-        blocActifY[0] = (int)TypeEtat.LIGNE;
-        blocActifX[0] = (int)TypeEtat.LIGNE;
-        blocActifX[1] = (int)TypeEtat.LIGNE;
-        blocActifX[2] = (int)TypeEtat.LIGNE;
-        blocActifX[3] = (int)TypeEtat.LIGNE;
-      }
-      else if (randomPiece == 4) //Si bloc T
-      {
 
         // <scloutier>
         int GenererPieceAleatoire()
@@ -221,9 +191,9 @@ namespace TP3
                 blocActifX[1] = (int)TypeEtat.T;
                 blocActifX[2] = (int)TypeEtat.T;
 
-      }
-      else //Si bloc S
-      {
+            }
+            else if (randomPiece == 5) //Si bloc S
+            {
                 //PositionY
                 blocActifY[0] = (int)TypeEtat.L;
                 blocActifY[1] = (int)TypeEtat.L;
@@ -234,8 +204,10 @@ namespace TP3
                 blocActifX[0] = (int)TypeEtat.L;
                 blocActifX[0] = (int)TypeEtat.L;
                 blocActifX[1] = (int)TypeEtat.L;
-      }
-                //PositionY
+            }
+            else if (randomPiece == 6) //Si bloc L
+            //PositionY
+            {
                 blocActifY[0] = (int)TypeEtat.L;
                 blocActifY[1] = (int)TypeEtat.L;
                 blocActifY[1] = (int)TypeEtat.L;
@@ -245,9 +217,10 @@ namespace TP3
                 blocActifX[0] = (int)TypeEtat.L;
                 blocActifX[1] = (int)TypeEtat.L;
                 blocActifX[2] = (int)TypeEtat.L;
-      return randomPiece;
-    }
-                //PositionY
+            }
+            else if (randomPiece == 7) //Si bloc carré
+            //PositionY
+            {
                 blocActifY[0] = (int)TypeEtat.L;
                 blocActifY[1] = (int)TypeEtat.L;
                 blocActifY[1] = (int)TypeEtat.L;
@@ -257,7 +230,7 @@ namespace TP3
                 blocActifX[0] = (int)TypeEtat.L;
                 blocActifX[1] = (int)TypeEtat.L;
                 blocActifX[1] = (int)TypeEtat.L;
-    //<scloutier>
+            }
             else //Si bloc Z
             {
                 //PositionY
@@ -272,50 +245,53 @@ namespace TP3
                 blocActifX[1] = (int)TypeEtat.L;
             }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="nbLignes"></param>
-    /// <param name="nbColonnes"></param>
-    public void GenererTableauEtat(int nbLignes, int nbColonnes)
-    {
-      tableauEtats = new int[nbLignes, nbColonnes];
-
-      for (int i = 0; i < nbLignes; i++)
-      {
-        for (int j = 0; j < nbColonnes; j++)
-        {
-          tableauEtats[i, j] = (int)TypeEtat.NONE; // Initialisation au type -> None
+            return randomPiece;
         }
-      }
-    }
 
-    public void EffacerLigne(int nbDeLaLigneComplete)
-    {
-      for (int i = 0; i < tableauEtats.GetLength(1); i++)
-      {
-        tableauEtats[nbDeLaLigneComplete, i] = (int)TypeEtat.NONE;
-      }
-    }
-
-    public int VerifierLigne()
-    {
-      int nbLigneComplete;
-
-      for (int i = 0; i < tableauEtats.GetLength(0); i++)
-      {
-        for (int j = 0; j < tableauEtats.GetLength(1); j++)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nbLignes"></param>
+        /// <param name="nbColonnes"></param>
+        public void GenererTableauEtat(int nbLignes, int nbColonnes)
         {
-          if (tableauEtats)
+            tableauEtats = new int[nbLignes, nbColonnes];
+
+            for (int i = 0; i < nbLignes; i++)
+            {
+                for (int j = 0; j < nbColonnes; j++)
+                {
+                    tableauEtats[i, j] = (int)TypeEtat.NONE; // Initialisation au type -> None
+                }
+            }
         }
-      }
+
+        public void EffacerLigne(int nbDeLaLigneComplete)
+        {
+            for (int i = 0; i < tableauEtats.GetLength(1); i++)
+            {
+                tableauEtats[nbDeLaLigneComplete, i] = (int)TypeEtat.NONE;
+            }
+        }
+
+        public int VerifierLigne()
+        {
+            int nbLigneComplete;
+
+            for (int i = 0; i < tableauEtats.GetLength(0); i++)
+            {
+                for (int j = 0; j < tableauEtats.GetLength(1); j++)
+                {
+                    if (tableauEtats)
+                }
+            }
+        }
+
+
+        void RotationPiece()
+        {
+
+        }
+
     }
-
-
-    void RotationPiece()
-    {
-
-    }
-
-  }
 }
