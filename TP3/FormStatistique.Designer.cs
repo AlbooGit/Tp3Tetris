a7c793bd52791e41fffd95176383b70f5a65a121
Keatrix <samuel.cloutier21@gmail.com>
2016-12-07 03:47:42 -0500

    ffgdfdg

--- TP3/FormStatistique.Designer.cs
+++ TP3/FormStatistique.Designer.cs
@@ -29,47 +29,300 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblnbDeZ = new System.Windows.Forms.Label();
+            this.lblnbDeS = new System.Windows.Forms.Label();
+            this.lblnbDeJ = new System.Windows.Forms.Label();
+            this.lblnbDeT = new System.Windows.Forms.Label();
+            this.lblnbDeL = new System.Windows.Forms.Label();
+            this.lblnbDeLigne = new System.Windows.Forms.Label();
+            this.lblnbDeCarre = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label17);
+            this.groupBox1.Controls.Add(this.label16);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.lblnbDeZ);
+            this.groupBox1.Controls.Add(this.lblnbDeS);
+            this.groupBox1.Controls.Add(this.lblnbDeJ);
+            this.groupBox1.Controls.Add(this.lblnbDeT);
+            this.groupBox1.Controls.Add(this.lblnbDeL);
+            this.groupBox1.Controls.Add(this.lblnbDeLigne);
+            this.groupBox1.Controls.Add(this.lblnbDeCarre);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.ForeColor = System.Drawing.Color.Maroon;
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(259, 236);
+            this.groupBox1.Size = new System.Drawing.Size(443, 519);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Statistiques de la Partie";
             // 
-            // textBox1
+            // label8
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(121, 31);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(41, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 199);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 13);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Nombre de Z :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Nombre de S :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Nombre de L :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Nombre de J :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Nombre de T :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Nombre de Ligne :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(17, 63);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Nombre de Carré :";
+            // 
+            // lblnbDeZ
+            // 
+            this.lblnbDeZ.AutoSize = true;
+            this.lblnbDeZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnbDeZ.Location = new System.Drawing.Point(170, 199);
+            this.lblnbDeZ.Name = "lblnbDeZ";
+            this.lblnbDeZ.Size = new System.Drawing.Size(35, 17);
+            this.lblnbDeZ.TabIndex = 22;
+            this.lblnbDeZ.Text = "0 %";
+            // 
+            // lblnbDeS
+            // 
+            this.lblnbDeS.AutoSize = true;
+            this.lblnbDeS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnbDeS.Location = new System.Drawing.Point(169, 175);
+            this.lblnbDeS.Name = "lblnbDeS";
+            this.lblnbDeS.Size = new System.Drawing.Size(35, 17);
+            this.lblnbDeS.TabIndex = 21;
+            this.lblnbDeS.Text = "0 %";
+            // 
+            // lblnbDeJ
+            // 
+            this.lblnbDeJ.AutoSize = true;
+            this.lblnbDeJ.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnbDeJ.Location = new System.Drawing.Point(170, 130);
+            this.lblnbDeJ.Name = "lblnbDeJ";
+            this.lblnbDeJ.Size = new System.Drawing.Size(35, 17);
+            this.lblnbDeJ.TabIndex = 20;
+            this.lblnbDeJ.Text = "0 %";
+            // 
+            // lblnbDeT
+            // 
+            this.lblnbDeT.AutoSize = true;
+            this.lblnbDeT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnbDeT.Location = new System.Drawing.Point(170, 106);
+            this.lblnbDeT.Name = "lblnbDeT";
+            this.lblnbDeT.Size = new System.Drawing.Size(35, 17);
+            this.lblnbDeT.TabIndex = 19;
+            this.lblnbDeT.Text = "0 %";
+            // 
+            // lblnbDeL
+            // 
+            this.lblnbDeL.AutoSize = true;
+            this.lblnbDeL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnbDeL.Location = new System.Drawing.Point(170, 153);
+            this.lblnbDeL.Name = "lblnbDeL";
+            this.lblnbDeL.Size = new System.Drawing.Size(35, 17);
+            this.lblnbDeL.TabIndex = 18;
+            this.lblnbDeL.Text = "0 %";
+            // 
+            // lblnbDeLigne
+            // 
+            this.lblnbDeLigne.AutoSize = true;
+            this.lblnbDeLigne.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnbDeLigne.Location = new System.Drawing.Point(170, 83);
+            this.lblnbDeLigne.Name = "lblnbDeLigne";
+            this.lblnbDeLigne.Size = new System.Drawing.Size(35, 17);
+            this.lblnbDeLigne.TabIndex = 17;
+            this.lblnbDeLigne.Text = "0 %";
+            // 
+            // lblnbDeCarre
+            // 
+            this.lblnbDeCarre.AutoSize = true;
+            this.lblnbDeCarre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnbDeCarre.Location = new System.Drawing.Point(170, 59);
+            this.lblnbDeCarre.Name = "lblnbDeCarre";
+            this.lblnbDeCarre.Size = new System.Drawing.Size(35, 17);
+            this.lblnbDeCarre.TabIndex = 16;
+            this.lblnbDeCarre.Text = "0 %";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(140, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0 secondes";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 34);
+            this.label1.Location = new System.Drawing.Point(35, 236);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Durée de la Partie :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(140, 32);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(194, 13);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Nombre de blocs de chaque type";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(266, 65);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 31;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(266, 88);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 13);
+            this.label12.TabIndex = 32;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(266, 111);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(41, 13);
+            this.label13.TabIndex = 33;
+            this.label13.Text = "label13";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(266, 134);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 13);
+            this.label14.TabIndex = 34;
+            this.label14.Text = "label14";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(266, 157);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(41, 13);
+            this.label15.TabIndex = 35;
+            this.label15.Text = "label15";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(266, 180);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(41, 13);
+            this.label16.TabIndex = 36;
+            this.label16.Text = "label16";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(266, 203);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(41, 13);
+            this.label17.TabIndex = 37;
+            this.label17.Text = "label17";
             // 
             // FormStatistique
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(468, 544);
             this.Controls.Add(this.groupBox1);
             this.ForeColor = System.Drawing.Color.Black;
             this.Name = "FormStatistique";
@@ -84,6 +337,28 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblnbDeZ;
+        private System.Windows.Forms.Label lblnbDeS;
+        private System.Windows.Forms.Label lblnbDeJ;
+        private System.Windows.Forms.Label lblnbDeT;
+        private System.Windows.Forms.Label lblnbDeL;
+        private System.Windows.Forms.Label lblnbDeLigne;
+        private System.Windows.Forms.Label lblnbDeCarre;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
     }
 }
--- TP3/FormStatistique.cs
+++ TP3/FormStatistique.cs
@@ -18,15 +18,44 @@ namespace TP3
         {
             InitializeComponent();
             this.principal = formPrincipal;
+            label2.Text = principal.nbDeSeconde.ToString() + " secondes";
+
+            label11.Text = principal.nbDeCarre.ToString();
+            lblnbDeCarre.Text = principal.nbDeCarrePourcentage.ToString() + " %";
+
+            label12.Text = principal.nbDeLigne.ToString();
+            lblnbDeLigne.Text = principal.nbDeLignePourcentage.ToString() + " %";
+
+            label13.Text = principal.nbDeT.ToString();
+            lblnbDeT.Text = principal.nbDeTPourcentage.ToString() + " %";
+
+            label14.Text = principal.nbDeJ.ToString();
+            lblnbDeJ.Text = principal.nbDeJPourcentage.ToString() + " %";
+
+            label15.Text = principal.nbDeL.ToString();
+            lblnbDeL.Text = principal.nbDeLPourcentage.ToString() + " %";
+
+            label16.Text = principal.nbDeS.ToString();
+            lblnbDeS.Text = principal.nbDeSPourcentage.ToString() + " %";
+
+            label17.Text = principal.nbDeZ.ToString();
+            lblnbDeZ.Text = principal.nbDeZPourcentage.ToString() + " %";
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if(principal.nbDeSeconde >= 60)
-            {
-                principal.nbDeSeconde = principal.nbDeSeconde / 60;
-                textBox1.Text = principal.nbDeSeconde.ToString();
-            }
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
